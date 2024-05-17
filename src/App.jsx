3d535d4ecaf8233aa115d36f0b29cf25799986c3
Doanh <nhatdoanhnguyen@gmail.com>
2024-05-17 20:37:05 +0700

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,20 @@ function App() {
   return (
     <>
       <h2>Add a border to a table:</h2>
-
       <table>
         <tr>
           <th>Firstname</th>
           <th>Lastname</th>
-       
+        </tr>
+        <tr>
+          <td>Peter</td>
+          <td>Griffin</td>
+        </tr>
+        <tr>
+          <td>Lois</td>
+          <td>Griffin</td>
+        </tr>
+      </table>
     </>
   );
 }
